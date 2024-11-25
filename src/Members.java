a5f7b874bc7b66da016566203ebf1d88c11f388c
Kurdoz1999 <roek0001@stud.kea.dk>
2024-11-25 16:36:03 +0100

    Ændret koden en smule, men der er fortsat fejl i "Annaual fee" når "Member" er <60

--- src/Members.java
+++ src/Members.java
@@ -14,7 +14,7 @@ public class Members {
     private final int juniorRate = 1000;
     private final int seniorRate = 1600;
     private final int seniorDiscount = 25;
-    private final int seniorDiscountedRate = (seniorRate-((100-seniorDiscount)/100));
+    private final int seniorDiscountedRate = seniorRate-(1-(seniorDiscount/100));
     private final int passiveRate = 500;
     private ArrayList<Members> members = new ArrayList();
 
@@ -44,12 +44,10 @@ public class Members {
             if (isActive == true) {
                 if (age < 18) {
                     setAnnualFee(juniorRate);
-                } else if (age >= 18) {
-                    if (age > 60) {
-                        setAnnualFee(seniorDiscountedRate);
-                    } else {
-                        setAnnualFee(seniorRate);
-                    }
+                } else if (age < 60) {
+                    setAnnualFee(seniorDiscountedRate);
+                } else if (age >= 60){
+                    setAnnualFee(seniorRate);
                 }
             } else if (isActive == false) {
                 setAnnualFee(passiveRate);
