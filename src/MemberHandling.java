8eacd4ca9aa14a2eccd7a9830ae5f9ddb9dd8825
Enes-Lenovo <enfi0001@stud.kea.dk>
2024-11-26 11:55:51 +0100

    AddMember funktion tilføjet. Mangler stadig at få rettet fejl

--- src/MemberHandling.java
+++ src/MemberHandling.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class MemberHandling {
     private ArrayList<Members> members = new ArrayList();
 
-
+//abdyk
     public void addMember(Members member){
         members.add(member);
     }
