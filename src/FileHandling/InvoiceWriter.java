65944a1c025d298bd2931f474febbde2dd2439fe
XanderKjoller <xander@kjollers.dk>
2024-12-10 12:06:37 +0100

    Invoices

--- src/FileHandling/InvoiceWriter.java
+++ src/FileHandling/InvoiceWriter.java
@@ -13,8 +13,8 @@ public class InvoiceWriter {
     public String debtInvoice(Member m)
     {
         try {
-            //FileWriter writer = new FileWriter("src/Files/Invoices/"+ m.getName(.txt));
-            FileWriter writer = new FileWriter("Files/Invoices/"+ m.getName()+".txt");
+            FileWriter writer = new FileWriter("src/Files/Invoices/"+ m.getName()+".txt");
+            //FileWriter writer = new FileWriter("Files/Invoices/"+ m.getName()+".txt");
 
             writer.write("hi " + m.getName() + " We noticed that you have yet to pay your annual membership fee, and as such have a outstanding of "+ m.getDebt() + "dkk. " +
                     "\n please pay your outstanding before the end of the month");
